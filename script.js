1b6e1eb9788ca58c780a6fd039ac8211b411ca73
Joakim Edwardh <kempanedwardh@hotmail.com>
2018-03-10 16:23:47 +0100

    update

---
+++ script.js
@@ -0,0 +1,79 @@
+
+$(document).ready(function(){
+            
+    //Skriver ut horoskopet om det finns sparat i SESSION
+    viewHoroscope = function(){
+        $.ajax({
+            url:"viewHoroscope.php",
+            method: "GET",
+            success: function(results){
+                $(".content").html(results);
+            }
+        });
+    }
+
+    viewHoroscope();
+
+    //Visar sparat horoskop
+    $("#visaHoroskop").click(function(){
+        viewHoroscope();
+    });
+    
+    //Sparar horoskopet i SESSION och skriver ut om SESSION är tomt
+    //Om det redan finns ett horoskop sparat i SESSION säger den ifrån
+    $("#sparaHoroscope").click(function(){
+                
+        $.ajax({
+            url:"addHoroscope.php",
+            method: "POST",
+            data:{
+                "personNr": $("#angivetNummer").val()
+            },
+            success: function(results){
+                if(results == "true"){
+                    viewHoroscope();
+                }
+                
+                else { 
+                    $(".content").html(results);
+                }
+            }
+        });
+    });
+    
+    //Uppdaterar SESSION med det nya horoskopet och skriver ut
+    //Ber dig sparat ett horoskop om SESSION är tomt
+    $("#uppdateraHoroscope").click(function(){
+                
+        $.ajax({
+            url:"updateHoroscope.php",
+            method: "PUT",
+            data:{
+                "personNr": $("#angivetNummer").val()
+            },
+            success: function(results){
+                if(results == "true"){
+                    viewHoroscope();
+                } else {
+                    $(".content").html(results);
+                }
+            }
+        }); 
+    });
+    
+    //Tömmer SESSION 
+    //Säger ifrån om det inte finns något sparat
+    $("#raderaHoroscope").click(function(){
+                
+        $.ajax({
+            url:"deleteHoroscope.php",
+            method: "DELETE",
+            data:{
+                "personNr": $("#angivetNummer").val()
+            },
+            success: function(results){
+                $(".content").html(results);
+            }
+        });   
+    });
+});
